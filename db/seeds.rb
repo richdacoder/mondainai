f0008064eb8e5ab999dd8f3aa09fbca7452fe89c
Alonzo Anderson <alonzoanderson29@gmail.com>
2025-08-26 16:32:10 +0900

    User seeds done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require "faker"
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -7,3 +8,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Message.destroy_all
+User.destroy_all
+Item.destroy_all
+Request.destroy_all
+
+puts "Creating users..."
+users = []
+ 5.times do
+  users << User.create!(
+    email: Faker::Internet.unique.email,
+    password: "password123",
+    password_confirmation: "password123",
+    name: Faker::App.name,
+    phone_number: Faker::PhoneNumber.phone_number
+  )
+end
+
+puts "Users created!"
