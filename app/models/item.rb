2e5a4c6511fa892315837da760566d1651f254d2
Alonzo Anderson <alonzoanderson29@gmail.com>
2025-08-26 15:05:42 +0900

    Item Associations and Validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :photo, presence: true
+  enum status: { draft: 0, live: 1, claimed: 3 }
+  validates :weight, numericality: { only_integer: true }
+  validates :location, presence: true
 end
