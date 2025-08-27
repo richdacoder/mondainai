f004773c9854193835fd13015cced84d12ddb8a8
Andrew Leemhuis <tanukilandslide@gmail.com>
2025-08-27 16:23:55 +0900

    fixed seeds to work with Cloudinary

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,6 @@ class Item < ApplicationRecord
   has_many :requests
   validates :name, presence: true
   validates :description, presence: true
-  validates :photo, presence: true
   enum status: { draft: 0, live: 1, pending: 2, claimed: 3 }
   validates :weight, numericality: { only_integer: true }
 end
