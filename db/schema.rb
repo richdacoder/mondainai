2d8c1ca73c7e3c37cd81ed9abc26568fe6bbc0da
Alonzo Anderson <alonzoanderson29@gmail.com>
2025-08-27 12:06:14 +0900

    Added address to User Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_26_054632) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_27_030344) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,7 +50,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_26_054632) do
     t.date "available_date"
     t.date "deadline_date"
     t.string "dimensions"
-    t.integer "weight"
+    t.float "weight"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -90,6 +90,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_26_054632) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
