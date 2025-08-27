eddd6b675c759a58cac6aafda81a5b739b2ecad3
Alonzo Anderson <alonzoanderson29@gmail.com>
2025-08-27 11:40:33 +0900

    Changed eight Integer To Float on Items Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_26_054632) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_27_022842) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,7 +50,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_26_054632) do
     t.date "available_date"
     t.date "deadline_date"
     t.string "dimensions"
-    t.integer "weight"
+    t.float "weight"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
