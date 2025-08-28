dc74c92c694c685054ee0fcd4a05e0abe7cc8f1d
Alonzo Anderson <alonzoanderson29@gmail.com>
2025-08-28 13:49:54 +0900

    Toggle Feature finished

---
+++ app/javascript/controllers/toggle_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="toggle"
+export default class extends Controller {
+  static targets = ["label"]
+  changeText(event) {
+  // If the checkbox is checked, change the label text to items held
+  // else, if the checkbox is unchecked, then change the label text to hold item
+  if (event.currentTarget.checked) {
+    this.labelTarget.innerText = "Items Held"
+  } else {
+    this.labelTarget.innerText = "Hold Item"
+  }
+  }
+  connect() {
+  }
+}
