731f522ff30fdcab4c6c060211d75a20e129d529
richard williams <richard.williams5697@yahoo.com>
2025-08-28 09:59:52 +0900

    made create for messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,11 @@
 class MessagesController < ApplicationController
+  def create
+    @message = Message.new(message_params)
+  end
+
+  private
+
+  def message_params
+    param.require(:item).permit(:content)
+  end
 end
