097c235fd5ca35624bc4f85355f9b3e7e44cf8d2
Andrew Leemhuis <tanukilandslide@gmail.com>
2025-08-28 17:51:32 +0900

    User is only able to see requests made to them

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,7 +1,7 @@
 class RequestsController < ApplicationController
   def index
-    @requests = Request.all
-    # @item = Request.find(params[:id]).item
+    @requests_received = current_user.requests_as_owner
+    @requests_sent = current_user.requests
   end
 
   def show
