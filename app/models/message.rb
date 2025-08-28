6c6d4887a9ebc85f1bbfbabf43db1d08584f1d62
richard williams <richard.williams5697@yahoo.com>
2025-08-28 18:51:53 +0900

    had problems with rails messaging system so moved into javascript

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,11 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :request
   validates :content, presence: true
+  after_create_commit :broadcast_append_to_request
+
+  private
+
+  def broadcast_append_to_request
+    broadcast_append_to request, target: "messages", partial: "messages/message", locals: { message: self }
+  end
 end
