584b795bc706cd7cd4c4959edf661161cbbbeefe
richard williams <richard.williams5697@yahoo.com>
2025-09-01 14:53:18 +0900

    added preferred language in users and translated content in  messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_28_075754) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_01_052541) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,6 +62,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_28_075754) do
     t.bigint "request_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "translated_content"
     t.index ["request_id"], name: "index_messages_on_request_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
@@ -98,6 +99,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_28_075754) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "address"
+    t.string "preferred_language", default: "english"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
