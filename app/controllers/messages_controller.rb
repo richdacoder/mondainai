9524e165e87aa0bffa5da5d14438a58a6c109d23
richard williams <richard.williams5697@yahoo.com>
2025-09-01 16:42:43 +0900

    ready for ai created condition to translate

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,11 +3,20 @@ class MessagesController < ApplicationController
     @request = Request.find(params[:request_id])
     @message = @request.messages.new(message_params)
     @message.user = current_user
+    request__user_language = @request.user.preferred_language
+    item_user_language = @request.item.user.preferred_language
 
-  if @message.save
-  else
-    render :new
-  end
+
+    if @message.valid? && request__user_language != @item_user_languag
+
+      @translated_language = @message.user == @request.user ? item_user_language : request__user_language
+      prompt = "Translate this #{@message.content} from #{@message.user.preferred_language} to #{@translated_language}"
+    end
+
+    if @message.save
+    else
+      render :new
+    end
   end
 
   private
