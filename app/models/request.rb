5cde911a06a2e19ba4e42d78d4dd5e6e6c104dde
richard williams <richard.williams5697@yahoo.com>
2025-09-02 11:15:17 +0900

    user can send one request per item

--- app/models/request.rb
+++ app/models/request.rb
@@ -3,5 +3,6 @@ class Request < ApplicationRecord
   belongs_to :item
   has_many :messages
   validates :status, presence: true
+  validates :user, uniqueness: { scope: :item, message: "You have already requested this item." }
   enum status: { pending: 0, accepted: 1, rejected: 2, inquiry: 3 }
 end
