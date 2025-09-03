217013193d1fab83b546e44afed89c4ffca84311
Andrew Leemhuis <tanukilandslide@gmail.com>
2025-09-03 15:17:21 +0900

    Adjusted text bubble, added profile image, and adjusted posted date

--- app/javascript/controllers/set_sender_controller.js
+++ app/javascript/controllers/set_sender_controller.js
@@ -7,17 +7,9 @@ export default class extends Controller {
     console.log(this.containerTargets);
 
     if (localStorage.getItem("currentUserId") === this.element.dataset.senderId){
-      this.containerTargets.forEach((container)=>{
-        container.classList.add("justify-content-end")
-      })
-      this.bubbleTarget.classList.add("bg-white")
+      this.bubbleTarget.classList.add("chat-current-user")
     } else {
-      this.containerTargets.forEach((container)=>{
-        container.classList.add("justify-content-start")
-      })
-      this.bubbleTarget.classList.add("bg-black")
-      this.bubbleTarget.classList.add("text-white")
-
+      this.bubbleTarget.classList.add("chat-other-user")
     }
   }
 }
