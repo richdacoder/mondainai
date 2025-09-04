628eb3691fef7982705736b1052139faa029020e
Alonzo Anderson <alonzoanderson29@gmail.com>
2025-09-04 21:42:04 +0900

    enum contents have been made lower case

--- app/models/request.rb
+++ app/models/request.rb
@@ -4,5 +4,5 @@ class Request < ApplicationRecord
   has_many :messages
   validates :status, presence: true
   validates :user, uniqueness: { scope: :item, message: "You have already requested this item." }
-  enum status: { Pending: 0, Accepted: 1, Rejected: 2, Inquiry: 3 }
+  enum status: { pending: 0, accepted: 1, rejected: 2, inquiry: 3 }
 end
